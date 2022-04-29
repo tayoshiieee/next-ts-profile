92c57c067d1ec62d590c632e8d528774bc9a7ef7
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-04-29 16:26:48 +0900

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        about: "url('/images/bg-about.png')",
+        future: "url('/images/bg-future.png')",
+        futureSp: "url('/images/bg-future-sp.png')",
+        skills: "url('/images/bg-skills.png')",
+        skillsSp: "url('/images/bg-skills-sp.png')",
+      }),
+    },
+    letterSpacing: {
+      wider: '.05em',
+      widest: '.15em',
+    },
+    // fontFamily: {
+    //   noto: "Noto Serif JP",
+    // },
   },
   plugins: [],
+  variants: {
+    borderWidth: ['responsive', 'first', 'hover', 'focus'],
+  },
 }
